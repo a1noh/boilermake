a4f46852b52a21faea5c15cbdd5282bf9d9a9e6a
austin noh <noh1austin@gmail.com>
2020-01-25 18:35:55 -0500

    ??

---
+++ boilermake/public/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.listen(3000, function () {
+    console.log('listening at port 3000');
+})
+
+app.post("/first", function(req,res){
+  res.send("hello")
+
+})
+
+
